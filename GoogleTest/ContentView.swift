fb1ca83ae5c73a2b274d77acc1902bb39e21e238
diego.aragon@g-tec.io-at-805495149875 <diego_isw@hotmail.com>
2020-02-15 21:54:33 -0600

    Initial Commit

---
+++ GoogleTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GoogleTest
+//
+//  Created by Diego Aragón Duarte on 15/02/20.
+//  Copyright © 2020 Diego Aragón Duarte. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
